c84a58f2c74832aa9e04bc38c605814ab606813e
Dartameill <serggarelov@gmail.com>
2022-02-01 22:26:58 +0300

    Db, not all

--- src/test/java/MyDataBase.java
+++ src/test/java/MyDataBase.java
@@ -1,9 +1,4 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
 
-package Servlets;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,14 +10,10 @@ public class MyDataBase {
     private final String user = "postgres";
     private final String password = "88654732";
 
-    public MyDataBase() {
-    }
-
     public Connection connect() {
         Connection conn = null;
-
         try {
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:8888/postgres", "postgres", "88654732");
+            conn = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to the PostgreSQL server successfully.");
             String query = "UPDATE public.customers SET email = ? WHERE id= ?";
             PreparedStatement pstmt = conn.prepareStatement(query);
@@ -30,13 +21,9 @@ public class MyDataBase {
             pstmt.setInt(2, 2);
             pstmt.execute();
             System.out.println("Records inserted......");
-            conn.prepareCall("select * from public.customers WHERE id = ?").getResultSet();
-            pstmt.setInt(1, 1);
-            pstmt.execute();
         } catch (SQLException var4) {
             System.out.println(var4.getMessage());
         }
-
         return conn;
     }
 
